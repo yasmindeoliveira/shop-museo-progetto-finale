bc7725a22b6bb8049f079ed6395891427ced080d
AnukaJapa <japaridze.ana96@gmail.com>
2023-01-27 19:00:39 +0100

    aggiunto modello ProductResupplyView

---
+++ ShopMuseoProgettoFinale/Models/ProductResupplyView.cs
@@ -0,0 +1,8 @@
+﻿namespace ShopMuseoProgettoFinale.Models
+{
+    public class ProductResupplyView
+    {
+        public Resupply Resupply { get; set; }
+        public List<Product>? ProductList { get; set; }
+    }
+}
