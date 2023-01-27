239c1869107737bab76b8eeeb30c49bc0f69dbdf
Thomas Youssef <thomasyoussef.business@protonmail.com>
2023-01-27 09:03:57 +0100

    Creati i vari modelli

---
+++ ShopMuseoProgettoFinale/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace ShopMuseoProgettoFinale.Models {
+    public class Product {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class Purchase {
+        public int Id { get; set; }
+        public DateOnly Date { get; set; }
+        public Product PurchasedProduct { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class Resupply {
+        public int Id { get; set; }
+        public DateOnly Date { get; set; }
+        public Product PurchasedProduct { get; set; }
+        public int Quantity { get; set; }
+        public string SupplierName { get; set; }
+        public double Price { get; set; }
+    }
+}
