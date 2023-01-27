cc1b6e5d76c0758bc8d26c9592a5bf7fb98bc5d7
AnukaJapa <japaridze.ana96@gmail.com>
2023-01-27 19:01:53 +0100

    Aggiunto ProductPurchaseView

---
+++ ShopMuseoProgettoFinale/Models/ProductPurchaseView.cs
@@ -0,0 +1,8 @@
+﻿namespace ShopMuseoProgettoFinale.Models
+{
+    public class ProductPurchaseView
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
