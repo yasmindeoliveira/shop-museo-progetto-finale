e1e256d6d28a7dc043c3bdbc76ee71f71e22548d
Thomas Youssef <thomasyoussef.business@protonmail.com>
2023-01-27 10:58:36 +0100

    Aggiunta validation a tutti i modelli

--- ShopMuseoProgettoFinale/Models/Product.cs
+++ ShopMuseoProgettoFinale/Models/Product.cs
@@ -4,15 +4,19 @@ namespace ShopMuseoProgettoFinale.Models {
     public class Product {
         [Key]
         public int Id { get; set; }
+
         [MaxLength(100, ErrorMessage = "Il nome del prodotto non può superare una lunghezza di 100 caratteri.")]
         public string Name { get; set; }
+
         [MaxLength(1024, ErrorMessage = "La descrizione del prodotto non può superare una lunghezza di 1024 caratteri.")]
         public string Description { get; set; }
+
         [Url(ErrorMessage = "Il link all'immagine deve essere un URL formattato correttamente.")]
         [MaxLength(256, ErrorMessage = "Il link all'immagine non può essere più lungo di 256 caratteri.")]
         [EndsWith(".png", ".jpg", ".webp", ErrorMessage = "Il link deve finire con l'estensione di un'immagine.")]
         public string PictureUrl { get; set; }
-        [Range(0, double.MaxValue, ErrorMessage = "Il prezzo non può essere meno di zero.")]
+
+        [Range(0, double.MaxValue, ErrorMessage = "Il prezzo non può essere negativo.")]
         public double Price { get; set; }
     }
 }
