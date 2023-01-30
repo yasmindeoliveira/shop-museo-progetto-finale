8f5fa8d062f7289a3c33cb7013123238009f93a7
Thomas Youssef <thomasyoussef.business@protonmail.com>
2023-01-30 10:55:10 +0100

    Finito Index per tutti i prodotti per Admin

---
+++ ShopMuseoProgettoFinale/Views/Shared/_AdminProductCard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShopMuseoProgettoFinale.Views.Shared
+{
+    public class AdminPizzaCardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
