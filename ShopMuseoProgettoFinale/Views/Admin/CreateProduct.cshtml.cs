77c50f765e369b1497ba81eb732a9c9411334559
Thomas Youssef <thomasyoussef.business@protonmail.com>
2023-01-30 09:40:27 +0100

    Creata cartella Admin Views

---
+++ ShopMuseoProgettoFinale/Views/Admin/CreateProduct.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShopMuseoProgettoFinale.Views.Admin
+{
+    public class CreateProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
